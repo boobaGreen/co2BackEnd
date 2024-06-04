3939a2e505e6d2328ea5a4f574e6430e79249eca
boobaGreen <devboobagreen@gmail.com>
2024-06-04 03:09:27 +0200

    change some monoose report details

--- models/reportModel.js
+++ models/reportModel.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
 
 const reportSchema = new mongoose.Schema({
-  groupId: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Group',
-    required: [true, 'Report must belong to a Group.'],
-  }, // ID del gruppo Telegram
+  groupId: { type: String, required: [true, 'Report must belong to a Group.'] }, // String ID from Telegram
 
-  participants: { type: Number }, // Numero di partecipanti nel gruppo
-  totalMessages: { type: Number, required: true }, // Numero totale di messaggi inviati nell'ultima ora
-  totalSizeBytes: { type: Number, required: true }, // Dimensione totale dei messaggi inviati nell'ultima ora in byte
-  totalCO2: { type: Number, required: true }, // Emissioni totali di CO2 generate nell'ultima ora in grammi
-  timestamp: { type: Date, default: Date.now, required: true }, // Timestamp del report
+  // participants: { type: Number }, // Optional, remove if not needed
+
+  totalMessages: { type: Number, required: true }, // Number of messages
+
+  totalSizeKB: {
+    type: Number,
+    required: true,
+  },
+
+  emissionsOneByteMethod: { type: Number },
+  emissionsSWDMethod: { type: Number },
+
+  timestamp: { type: Date, default: Date.now, required: true }, // Timestamp of the report
 });
 
 const Report = mongoose.model('Report', reportSchema);
