b08d330ada2f08fe34fa8bf30287f6d36c1cd492
boobaGreen <devboobagreen@gmail.com>
2024-06-06 21:09:54 +0200

    render ok newwws

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const http = require('http');
 const dotenv = require('dotenv');
-const axios = require('axios');
 
 const { app } = require('./app');
 
@@ -10,47 +9,7 @@ dotenv.config({ path: './config.env' });
 const DB =
   'mongodb+srv://devboobagreen:QxJJgyTcZSR9Rd8b@co2telegram.hiwspma.mongodb.net/?retryWrites=true&w=majority&appName=Co2Telegram';
 
-async function updateWhitelist() {
-  try {
-    // Ottieni l'IP corrente
-    const response = await axios.get('https://api.ipify.org?format=json');
-    const currentIP = response.data.ip;
-
-    // API URL e credenziali per MongoDB Atlas
-    const atlasUrl = `https://cloud.mongodb.com/api/atlas/v1.0/groups/${process.env.ATLAS_PROJECT_ID}/accessList`;
-    const atlasUser = process.env.ATLAS_API_USER;
-    const atlasKey = process.env.ATLAS_API_KEY;
-
-    // Aggiungi il nuovo IP alla whitelist
-    const newIP = { ipAddress: currentIP };
-    const result = await axios.post(atlasUrl, newIP, {
-      auth: {
-        username: atlasUser,
-        password: atlasKey,
-      },
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (result.status === 201) {
-      console.log('IP aggiunto alla whitelist con successo!');
-    } else {
-      console.error(
-        "Errore nell'aggiungere IP alla whitelist:",
-        result.status,
-        result.data,
-      );
-    }
-  } catch (error) {
-    console.error(
-      "Errore nell'ottenere l'IP corrente o nell'aggiornare la whitelist:",
-      error.message,
-    );
-  }
-}
-
 async function dbConnect() {
-  await updateWhitelist();
-
   try {
     await mongoose.connect(DB);
     console.log('Connected to the database!');
