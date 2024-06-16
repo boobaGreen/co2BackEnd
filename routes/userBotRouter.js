87bcdd25b4bf09d477fb7911f8b348b28f4d25db
boobaGreen <devboobagreen@gmail.com>
2024-06-17 00:21:48 +0200

    testuserbotdetailgrouplocal

---
+++ routes/userBotRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const userBotController = require('../controllers/userBotController');
+// const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+// Routes for reports
+// router.route('/').get(testController.test);
+router.route('/').get(userBotController.test);
+
+module.exports = router;
