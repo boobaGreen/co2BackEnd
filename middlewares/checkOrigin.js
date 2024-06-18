df9b98c35b410353cffb8b6b7723f4e77e43af6d
boobaGreen <devboobagreen@gmail.com>
2024-06-19 01:35:33 +0200

    custom middleware check origin origin console log Origin

--- middlewares/checkOrigin.js
+++ middlewares/checkOrigin.js
@@ -4,7 +4,7 @@ const allowedOrigins = ['https://telegrambottest-eacl.onrender.com'];
 
 const checkOrigin = (req, res, next) => {
   if (req.method === 'POST') {
-    const origin = req.get('origin');
+    const origin = req.get('Origin');
     if (!allowedOrigins.includes(origin)) {
       console.log('Forbidden-origin :', origin);
       return res.status(403).json({ status: 'fail', message: 'Forbidden' });
