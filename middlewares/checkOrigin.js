221103ddc4ec411e76334efba17a03f1e9d87f38
boobaGreen <devboobagreen@gmail.com>
2024-06-19 01:46:30 +0200

    req debug aiuto

--- middlewares/checkOrigin.js
+++ middlewares/checkOrigin.js
@@ -4,7 +4,7 @@ const allowedOrigins = ['https://telegrambottest-eacl.onrender.com'];
 
 const checkOrigin = (req, res, next) => {
   if (req.method === 'POST') {
-    console.log('req°°°°°°°°°°°°°°', req);
+    console.log('req°°°°°°°°°°°°°°', req.headers);
     const origin = req.get('Origin');
     if (!allowedOrigins.includes(origin)) {
       console.log('Forbidden-origin :', origin);
