b6e31ed4e898c89809b7418ade17f2c22fbb0d52
boobaGreen <devboobagreen@gmail.com>
2024-06-18 14:37:59 +0200

    first get group details correct

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -10,10 +10,8 @@ router
   .get(groupController.getAllGroup) // per tutti - verifica filtri
   .post(groupController.createGroup); // solo per bot***da aggiungere auth
 
-router
-  .route('/:id')
-  .get(groupController.getGroup) // per tutti - verifica filtri
-  .patch(groupController.updateGroup) // solo per admin dell canale
-  .delete(groupController.deleteGroup); // solo per admin del canale
+router.route('/:id').get(groupController.getGroup); // per tutti - verifica filtri
+// .patch(groupController.updateGroup) // solo per admin dell canale
+// .delete(groupController.deleteGroup); // solo per admin del canale
 
 module.exports = router;
