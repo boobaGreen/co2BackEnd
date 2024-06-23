1966b5972cd36d9ae21a7e9d2c6447764c23604f
boobaGreen <devboobagreen@gmail.com>
2024-06-23 20:31:28 +0200

    secure ture 2

--- middlewares/checkOriginTelegram.js
+++ middlewares/checkOriginTelegram.js
@@ -1,11 +1,28 @@
-const allowedOrigins = ['secretorginipasswordtomorrowdevfromfe'];
+const allowedCustomOrigins = ['secretorginipasswordtomorrowdevfromfe'];
+const allowedRealOrigins = ['https://6b98-5-90-138-45.ngrok-free.app'];
 
 const checkOriginTelegram = (req, res, next) => {
   if (req.method === 'POST') {
-    const origin = req.get('X-Custom-Origin');
-    if (!origin || !allowedOrigins.includes(origin)) {
-      console.log('Forbidden origin:', origin);
-      return res.status(403).json({ status: 'fail', message: 'Forbidden' });
+    // Verifica l'intestazione personalizzata
+    const customOrigin = req.get('X-Custom-Origin');
+    console.log('Detected custom origin:', customOrigin);
+
+    // Verifica l'intestazione reale dell'origine
+    const realOrigin = req.get('Origin');
+    console.log('Detected real origin:', realOrigin);
+
+    if (!customOrigin || !allowedCustomOrigins.includes(customOrigin)) {
+      console.log('Forbidden custom origin:', customOrigin);
+      return res
+        .status(403)
+        .json({ status: 'fail', message: 'Forbidden custom origin' });
+    }
+
+    if (!realOrigin || !allowedRealOrigins.includes(realOrigin)) {
+      console.log('Forbidden real origin:', realOrigin);
+      return res
+        .status(403)
+        .json({ status: 'fail', message: 'Forbidden real origin' });
     }
   }
 
