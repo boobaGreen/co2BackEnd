1e6fd7c6714ec834a236bb38d5a0fd3a39bd3ce2
boobaGreen <devboobagreen@gmail.com>
2024-06-23 20:49:56 +0200

    aconra hmac

--- middlewares/checkOriginTelegram.js
+++ middlewares/checkOriginTelegram.js
@@ -1,5 +1,9 @@
+/* eslint-disable camelcase */
 const allowedCustomOrigins = ['secretorginipasswordtomorrowdevfromfe'];
 const allowedRealOrigins = ['https://6b98-5-90-138-45.ngrok-free.app'];
+
+const crypto = require('crypto');
+const AppError = require('../utils/appError');
 
 const checkOriginTelegram = (req, res, next) => {
   if (req.method === 'POST') {
@@ -23,6 +27,23 @@ const checkOriginTelegram = (req, res, next) => {
       return res
         .status(403)
         .json({ status: 'fail', message: 'Forbidden real origin' });
+    }
+
+    // Verifica HMAC
+    const { id, first_name, username, photo_url, auth_date, hash } = req.body;
+
+    // Calcola l'HMAC utilizzando la chiave segreta del tuo bot Telegram
+    const secret = 'your_telegram_bot_secret'; // Sostituisci con la tua chiave segreta del bot Telegram
+    const data = `${id}${first_name}${username}${photo_url}${auth_date}`;
+    const hmac = crypto.createHmac('sha256', secret);
+    hmac.update(data);
+    const calculatedHash = hmac.digest('hex');
+    console.log('calculatedHash : ', calculatedHash);
+    console.log('hash : ', hash);
+    if (hash !== calculatedHash) {
+      return next(
+        new AppError('Telegram authentication failed. Invalid HMAC.', 401),
+      );
     }
   }
 
