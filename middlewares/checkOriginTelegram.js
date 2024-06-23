f8fb90f717808ac4901f4675f61f617114f840b1
boobaGreen <devboobagreen@gmail.com>
2024-06-23 18:27:46 +0200

    rq.method check 2

--- middlewares/checkOriginTelegram.js
+++ middlewares/checkOriginTelegram.js
@@ -1,21 +1,23 @@
 const checkOriginTelegram = (req, res, next) => {
-  console.log('Request method:', req.method);
-  console.log('Headers:', req.headers);
+  if (req) {
+    console.log('Headers:', req.headers);
+    console.log('req.method:', req.method);
 
-  // Verifica l'intestazione personalizzata
-  const origin = req.get('X-Custom-Origin');
-  console.log('Detected origin **************:', origin);
+    // Verifica l'intestazione personalizzata
+    const origin = req.get('X-Custom-Origin');
+    console.log('Detected origin **************:', origin);
 
-  // Temporaneamente non blocca nessuna origine, solo logga
-  // const allowedOriginsTelegram = ['supersegretissimo'];
+    // Temporaneamente non blocca nessuna origine, solo logga
+    // const allowedOriginsTelegram = ['supersegretissimo'];
 
-  // if (!origin || !allowedOriginsTelegram.includes(origin)) {
-  //   console.log('Forbidden origin:', origin);
-  //   return res.status(403).json({ status: 'fail', message: 'Forbidden' });
-  // }
+    // if (!origin || !allowedOriginsTelegram.includes(origin)) {
+    //   console.log('Forbidden origin:', origin);
+    //   return res.status(403).json({ status: 'fail', message: 'Forbidden' });
+    // }
+  }
+
+  // Passa al middleware successivo
+  next();
 };
-
-// Passa al middleware successivo
-next();
 
 module.exports = checkOriginTelegram;
