b93beed7afcb107b99f28d9f255691a1d9b9c2ec
boobaGreen <devboobagreen@gmail.com>
2024-06-24 03:11:16 +0200

    notte notte

--- middlewares/authController.js
+++ middlewares/authController.js
@@ -52,18 +52,3 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   next();
 });
-
-exports.restrictTo = (...roles) => {
-  return (req, res, next) => {
-    // roles  ["admin","mod","user"]  role="user"
-
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new AppError('You do not have permission to perform this action', 403),
-      );
-    }
-    next();
-  };
-};
-
-// not used in StudyBuddyHub
