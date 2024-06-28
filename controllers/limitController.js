abe403d59d2e93c98c659ce84399ae1e08407221
boobaGreen <devboobagreen@gmail.com>
2024-06-28 03:26:47 +0200

    local ok 9

--- controllers/limitController.js
+++ controllers/limitController.js
@@ -3,27 +3,37 @@ const axios = require('axios');
 const Group = require('../models/groupModel');
 const isAdminMiddleware = require('../middlewares/isAdminMiddleware');
 
-exports.createLimitGeneric = async (req, res, next) => {
+exports.deleteLimitGeneric = async (req, res, next) => {
   try {
-    const { chatId, limit } = req.body;
-    if (!chatId || !limit) {
-      return res.status(400).json({ error: 'chatId e limit sono richiesti.' });
+    const { chatId } = req.params;
+    if (!chatId) {
+      return res.status(400).json({ error: 'chatId è richiesto.' });
     }
 
-    // Aggiorna il limite generico nel bot
-    const endpoint = `${process.env.BOT_API_URL}/groupLimitGeneric`;
-    const response = await axios.post(endpoint, { chatId, limit });
+    // Rimuovi il limite generico dal bot
+    const endpoint = `${process.env.BOT_API_URL}/groupLimitGeneric/${chatId}`;
+    const response = await axios.delete(endpoint);
 
-    // Aggiorna il modello Group con il nuovo limite
-    const updatedGroup = await Group.findOneAndUpdate(
-      { groupId: chatId },
-      { groupLimits: limit }, // Assegna direttamente il nuovo limite come numero
-      { new: true, upsert: true }, // Crea un nuovo documento se non esiste
-    );
+    if (response.status === 200 || response.status === 204) {
+      // Rimuovi il limite dal modello Group solo se l'operazione è andata a buon fine
+      await Group.findOneAndUpdate(
+        { groupId: chatId },
+        { groupLimits: -1 }, // Assegna un valore di default per indicare l'assenza di limite
+        { new: true }, // Ottieni il documento aggiornato
+      );
 
-    res.status(response.status).json(response.data);
+      res
+        .status(200)
+        .json({ status: 'success', message: 'Limit deleted successfully' });
+    } else {
+      // Se il server di backend non restituisce 204, gestisci l'errore di conseguenza
+      res.status(response.status).json(response.data);
+    }
   } catch (error) {
-    console.error('Errore durante la creazione del limite generico: ', error);
+    console.error(
+      'Errore durante la cancellazione del limite generico:',
+      error,
+    );
     next(error);
   }
 };
