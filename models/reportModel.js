05a07539ba469e9460a47b7e80f6fd6657aad63f
boobaGreen <devboobagreen@gmail.com>
2024-06-30 23:10:57 +0200

    newtype1 2

--- models/reportModel.js
+++ models/reportModel.js
@@ -13,119 +13,128 @@ const reportSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'Report must have a total kb'],
   },
-  emissionsOneByteMethod: { type: Number },
-  emissionsSWDMethod: { type: Number },
+  emissionsOneByteMethod: {
+    type: Number,
+    required: [true, 'Report must have a total emissionsOneByteMethod'],
+  },
+  emissionsSWDMethod: {
+    type: Number,
+    required: [true, 'Report must have a total emissionsSWDMethod'],
+  },
   textTotalMessages: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total text messages'],
   },
   textTotalSize: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total text kb'],
   },
   textEmissionsOneByteMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total text emissionsOneByteMethod'],
   },
   textEmissionsSWDMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total text emissionsSWDMethod'],
   },
   photoTotalMessages: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total photo messages'],
   },
   photoTotalSize: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total photo kb'],
   },
   photoEmissionsOneByteMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total photo emissionsOneByteMethod'],
   },
   photoEmissionsSWDMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total photo emissionsSWDMethod'],
   },
   voiceTotalMessages: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total voice messages'],
   },
   voiceTotalSize: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total voice kb'],
   },
   voiceEmissionsOneByteMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total voice emissionsOneByteMethod'],
   },
   voiceEmissionsSWDMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total voice emissionsSWDMethod'],
   },
   videoTotalMessages: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total video messages'],
   },
   videoTotalSize: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total video kb'],
   },
   videoEmissionsOneByteMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total video emissionsOneByteMethod'],
   },
   videoEmissionsSWDMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total video emissionsSWDMethod'],
   },
   documentTotalMessages: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total document messages'],
   },
   documentTotalSize: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total document kb'],
   },
   documentEmissionsOneByteMethod: {
     type: Number,
-    required: true,
+    required: [
+      true,
+      'Report must have a total document emissionsOneByteMethod',
+    ],
   },
   documentEmissionsSWDMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total document emissionsSWDMethod'],
   },
   pollTotalMessages: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total poll messages'],
   },
   pollTotalSize: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total poll kb'],
   },
   pollEmissionsOneByteMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total poll emissionsOneByteMethod'],
   },
   pollEmissionsSWDMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total poll emissionsSWDMethod'],
   },
   stickerTotalMessages: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total sticker messages'],
   },
   stickerTotalSize: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total sticker kb'],
   },
   stickerEmissionsOneByteMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total sticker emissionsOneByteMethod'],
   },
   stickerEmissionsSWDMethod: {
     type: Number,
-    required: true,
+    required: [true, 'Report must have a total sticker emissionsSWDMethod'],
   },
   timestamp: {
     type: Date,
